322d5a35304637f06d0ee295b7e8c1e065cf5007
aytona <christopher.aytona@gmail.com>
2016-05-20 23:49:55 -0400

    Behaviours II

--- WaveTest/SharedSource/Main/FollowBehaviour.cs
+++ WaveTest/SharedSource/Main/FollowBehaviour.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Text;
 using WaveEngine.Common.Attributes;
+using WaveEngine.Common.Math;
 using WaveEngine.Framework;
 using WaveEngine.Framework.Graphics;
 
@@ -18,9 +19,28 @@ namespace WaveTest
         [RenderPropertyAsEntity(new string[] { "WaveEngine.Framework.Graphics.Tranform3D" })]
         public string EntityPath { get; set; }
 
+        private Transform3D _target;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            if (string.IsNullOrEmpty(this.EntityPath))
+                return;
+
+            var entity = this.EntityManager.Find(this.EntityPath);
+            this._target = entity.FindComponent<Transform3D>();
+        }
+
         protected override void Update(TimeSpan gameTime)
         {
+            if (this._target == null)
+                return;
 
+            var lerp = Math.Min(1, 10 * (float)gameTime.TotalSeconds);
+
+            this._transform.Position = Vector3.Lerp(this._transform.Position, this._target.Position, lerp);
+            this._transform.Rotation = Vector3.Lerp(this._transform.Rotation, this._target.Rotation, lerp);
         }
     }
 }
