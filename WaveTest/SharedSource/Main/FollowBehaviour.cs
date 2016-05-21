973ce9bd4f2754d86a21b792e76d062bd7d46a30
aytona <christopher.aytona@gmail.com>
2016-05-20 23:34:08 -0400

    Remade ship material because it was giving an error

---
+++ WaveTest/SharedSource/Main/FollowBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using WaveEngine.Common.Attributes;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+
+namespace WaveTest
+{
+    [DataContract]
+    class FollowBehaviour : Behavior
+    {
+        [RequiredComponent]
+        public Transform3D _transform;
+
+        [DataMember]
+        [RenderPropertyAsEntity(new string[] { "WaveEngine.Framework.Graphics.Tranform3D" })]
+        public string EntityPath { get; set; }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+
+        }
+    }
+}
