af74e4aaa57bb3a202b64d196e4d79acd3be9372
aytona <christopher.aytona@gmail.com>
2016-05-21 23:55:49 -0400

    Initial behaviour III

---
+++ WaveTest/SharedSource/Main/AsteroidManager.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using WaveEngine.Common.Attributes;
+using WaveEngine.Components.Graphics3D;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Physics3D;
+
+namespace WaveTest
+{
+    [DataContract]
+    class AsteroidManager : Behavior
+    {
+        [DataMember]
+        public int NumberOfAsteroids { get; set; }
+
+        [DataMember]
+        public float AsteroidInterval { get; set; }
+
+        [DataMember]
+        [RenderPropertyAsEntity]
+        public string ShipPath { get; set; }
+
+        [DataMember]
+        public float AsteroidDistance { get; set; }
+
+        [DataMember]
+        public float AsteroidSpread { get; set; }
+
+        private bool isSpawned;
+        private List<Entity> asteroids;
+        private int asteroidIndex;
+        private float remainingAsteroidTime;
+        private Entity shipEntity;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            if (string.IsNullOrEmpty(this.ShipPath))
+                this.shipEntity = this.EntityManager.Find(this.ShipPath);
+        }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+            if (!this.isSpawned)
+            {
+                this.CreateAsteroid();
+                this.isSpawned = true;
+                return;
+            }
+
+            if (this.shipEntity == null && this.NumberOfAsteroids == 0)
+            {
+                return;
+            }
+
+            this.remainingAsteroidTime -= (float)gameTime.TotalSeconds;
+
+            if (this.remainingAsteroidTime <= 0)
+            {
+                this.ShowAsteroid();
+                this.remainingAsteroidTime += this.AsteroidInterval;
+            }
+        }
+
+        private void CreateAsteroid()
+        {
+            this.asteroids = new List<Entity>();
+            for(int i = 0; i < this.NumberOfAsteroids; i++)
+            {
+                var asteroid = this.CreateAsteroid(i);
+                this.asteroids.Add(asteroid);
+                this.EntityManager.Add(asteroid);
+            }
+            this.asteroidIndex = 0;
+        }
+
+        private Entity CreateAsteroid(int i)
+        {
+            string model;
+
+            switch (i % this.NumberOfAsteroids)
+            {
+                case 0:
+                    model = WaveContent.Assets.Models.asteroid_1_0_fbx;
+                    break;
+                case 1:
+                    model = WaveContent.Assets.Models.asteroid_2_0_fbx;
+                    break;
+                case 2:
+                    model = WaveContent.Assets.Models.asteroid_3_0_fbx;
+                    break;
+                default:
+                    model = WaveContent.Assets.Models.asteroid_4_0_fbx;
+                    break;
+            }
+
+            var asteroid = new Entity("asteroid" + i)
+                .AddComponent(new Transform3D())
+                .AddComponent(new Model(model))
+                .AddComponent(new MaterialsMap() { DefaultMaterialPath = WaveContent.Assets.Materials.AsteroidMat })
+                .AddComponent(new Spinner())
+                .AddComponent(new SphereCollider3D())
+                .AddComponent(new ModelRenderer());
+
+            asteroid.IsVisible = false;
+            return asteroid;
+        }
+
+        private void ShowAsteroid()
+        {
+            var asteroid = this.asteroids[this.asteroidIndex];
+            asteroid.IsVisible = true;
+
+            var shipTransform = this.shipEntity.FindComponent<Transform3D>();
+            var shipPosition = shipTransform.Position + (shipTransform.WorldTransform.Forward * this.AsteroidDistance);
+            shipPosition.X += WaveServices
+
+            this.asteroidIndex = (this.asteroidIndex + 1) % this.NumberOfAsteroids;
+        }
+    }
+}
